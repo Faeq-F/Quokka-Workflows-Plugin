9056b5d134c1205df17b318e4fad5e0be12adead
Faeq <faeqfaisal@hotmail.co.uk>
2025-11-01 11:01:39 +0000

    chore: :fix: :alien: checking for existence of windows before closing them & using UI Dispatcher to create Icon for items

--- WorkflowItem.cs
+++ WorkflowItem.cs
@@ -1,5 +1,6 @@
 ﻿using Quokka;
 using Quokka.ListItems;
+using Quokka.PluginArch;
 using System.Windows.Media.Imaging;
 
 namespace Plugin_Workflows {
@@ -9,21 +10,25 @@ namespace Plugin_Workflows {
 
     public Workflow(string command, List<string> flow) {
       this.flow = flow;
-      this.Name = command;
-      this.Description = $"The \"{command}\" workflow";
-      this.Icon = new BitmapImage(new Uri(
-          Environment.CurrentDirectory + "\\PlugBoard\\Plugin_Workflows\\Plugin\\chains.png"));
+      Name = command;
+      Description = $"The \"{command}\" workflow";
+      UiDispatcher.BeginInvoke(() => {
+        Icon = new BitmapImage(new Uri(
+            Environment.CurrentDirectory + "\\PlugBoard\\Plugin_Workflows\\Plugin\\chains.png"));
+      });
     }
 
     public override void Execute() {
       foreach (string command in this.flow) {
         Quokka.SearchWindow.ProduceItems(command)[0].Execute();
         //items (most likely) close the search window on execute
-        App.Current.MainWindow = new SearchWindow();
-        App.Current.MainWindow.Show();
+        if (App.Current.MainWindow == null) {
+          App.Current.MainWindow = new SearchWindow();
+          App.Current.MainWindow.Show();
+        }
       }
       //close final window that was launched
-      App.Current.MainWindow.Close();
+      if (App.Current.MainWindow != null) App.Current.MainWindow.Close();
     }
   }
 }
